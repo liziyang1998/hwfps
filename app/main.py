6ac0907c7b0d8a2ec8d0d43d6937c74f1e7df32d
liziyang <liziyang1998@gmail.com>
2024-03-28 01:00:49 +0800

    添加80端口

--- app/main.py
+++ app/main.py
@@ -64,5 +64,4 @@ def getHwScore():
     return GetHwScoreByPOST(data)
 
 if __name__ == "__main__":
-    # app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
-    app.run()
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
