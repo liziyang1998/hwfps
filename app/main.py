aadb9d82cb58ca2cfd2ba17020e0d60680cf246a
liziyang <liziyang1998@gmail.com>
2024-03-28 23:43:18 +0800

    添加前端

--- app/main.py
+++ app/main.py
@@ -12,20 +12,62 @@ def hello_world():
     """
     return render_template('index.html')
 
+def errorCheck(seasonData):
+    try:
+        int(seasonData[0])
+    except Exception as e:
+        return 'input error: 平台类型' + str(e)
+    if int(seasonData[0]) < 0:
+        return 'input error: 平台类型未选择'
+    
+    try:
+        float(seasonData[1])
+    except Exception as e:
+        return 'input error: 天梯分未输入或' + str(e)
+    
+    try:
+        float(seasonData[2])
+    except Exception as e:
+        return 'input error: rating未输入或' + str(e)
+
+    try:
+        float(seasonData[3])
+    except Exception as e:
+        return 'input error: rws/we未输入或' + str(e)
+
+    try:
+        float(seasonData[4])
+    except Exception as e:
+        return 'input error: adr未输入或' + str(e)
+
+    try:
+        float(seasonData[5])
+    except Exception as e:
+        return 'input error: 场次未输入或' + str(e)
+
+    try:
+        float(seasonData[6])
+    except Exception as e:
+        return 'input error: 胜率未输入或' + str(e)
+
+    return None
 
 @app.route('/getHwScore', methods=["POST"])
 def getHwScore():
     name = request.json.get("name")
     id = request.json.get("id")
-    season = request.json.get("season")
-    if name == None or id == None or season == None:
+    seasonStr = request.json.get("season")
+    if name == None or id == None or seasonStr == None:
         return 'input error, name/id/season is None'
     if type(name) != str:
         return 'input error, name is not str'
     if type(id) != str:
         return 'input error, id is not str'
-    if type(season) != int:
-        return 'input error, season is not int'
+    try:
+        int(seasonStr)
+    except Exception as e:
+        return 'input error: season未输入或不是整数'
+    season = int(seasonStr)
     
     platyType = []
     score = []
@@ -41,16 +83,9 @@ def getHwScore():
     if type(seasonData) != list:
         return 'input error, seasonData is not list'
     for i in range(0, season):
-        try:
-            int(seasonData[i][0])
-            float(seasonData[i][1])
-            float(seasonData[i][2])
-            float(seasonData[i][3])
-            float(seasonData[i][4])
-            float(seasonData[i][5])
-            float(seasonData[i][6])
-        except Exception as e:
-            return 'input error: ' + str(e)
+        errorLog = errorCheck(seasonData[i])
+        if errorLog != None:
+            return errorLog
         platyType.append(seasonData[i][0])
         score.append(seasonData[i][1])
         rating.append(seasonData[i][2])
